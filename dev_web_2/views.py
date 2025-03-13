091d877338e9e6b3ff7b62dc81246b9b49598424
Filipe Paixão <fpedrapaixao@gmail.com>
2025-03-13 15:23:57 -0300

    feat: add new def for pessoas

--- dev_web_2/views.py
+++ dev_web_2/views.py
@@ -3,3 +3,19 @@ from django.shortcuts import render
 
 def testRender(request):
     return render(request, 'first.html')
+
+def pessoas(request):
+    pessoas = [{
+        "name": "John",
+        "age": 30,
+        "city": "New York"
+    }, {
+        "name": "Peter",
+        "age": 45,
+        "city": "London"
+    }, {
+        "name": "Sally",
+        "age": 25,
+        "city": "Paris"
+    }]
+    return render(request, 'pessoas.html', {'pessoas': pessoas})
